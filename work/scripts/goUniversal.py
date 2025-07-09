e82b6ae656448adbbba2e68107ac8d28e11e0c43
Antoine Belley <antoine.belley@me.com>
2025-07-09 13:36:17 -0700

    Changed back the CMakeLists.txt to allow to buid from parent

--- work/scripts/goUniversal.py
+++ work/scripts/goUniversal.py
@@ -49,17 +49,16 @@ ELEM = ['n', 'H', 'He', 'Li', 'Be', 'B', 'C', 'N',
 ARGS = {}
 
 # Maximum value of s, and maximum step size ds
-ARGS['smax'] = '0'
+ARGS['smax'] = '500'
 ARGS['dsmax'] = '0.5'
-ARGS['basis'] = "oscillator"
-# ARGS['lmax3'] = '10' # for comparing with Heiko
+ARGS['basis'] = "NAT"
+ARGS['NAT_order'] = 'energy'
+# ARGS['use_NAT_occupations'] = 'true'  # Use NAT occupations for the valence space
 
 # Norm of Omega at which we split off and start a new transformation
 ARGS['omega_norm_max'] = '0.25'
 
-# Model space parameters used for reading Darmstadt-style interaction files
-ARGS['file2e1max'] = '18 file2e2max=36 file2lmax=18'
-ARGS['file3e1max'] = '18 file3e2max=36 file3e3max=24'
+
 
 # Name of a directory to write Omega operators so they don't need to be stored in memory. If not given, they'll just be stored in memory.
 # ARGS['scratch'] = 'SCRATCH'
@@ -116,17 +115,29 @@ time srun %s
 # Make a directory for the log files, if it doesn't already exist
 # if not path.exists('imsrg_log'): mkdir('imsrg_log')
 
+
+#Set the interaction files 
+ARGS['2bme'] = '/Users/antoinebelley/Documents/TRIUMF/Interactions/TwBME-HO_NN-only_N3LO_EM500_srg1.80_hw16_emax18_e2max36.me2j.gz'
+ARGS['3bme'] = '/Users/antoinebelley/Documents/TRIUMF/Interactions/NO2B_ThBME_EM1.8_2.0_3NFJmax15_IS_hw16_ms18_36_24.stream.bin'
+ARGS['LECs'] = 'EM1.8_2.0'
+# Model space parameters used for reading Darmstadt-style interaction files
+ARGS['file2e1max'] = '18 file2e2max=36 file2lmax=18'
+ARGS['file3e1max'] = '18 file3e2max=36 file3e3max=24'
+ARGS["3bme_type"] = "no2b"
+# ARGS["no2b_precision"] = "half"
+
 # Loop over multiple jobs to submit
-for Z in range(4, 5):
- A = 10
+for Z in range(7, 8):
+ A = 14
  for reference in ['%s%d' % (ELEM[Z], A)]:
   ARGS['reference'] = reference
   print('Z = ', Z)
-  for e in [4]:
+  for e in [10]:
    for hw in [16]:
 
      ARGS['emax'] = '%d' % e
-     ARGS['e3max'] = '12'
+     ARGS['e3max'] = '18'
+     ARGS['emax_imsrg'] = 8
 
      # Delta
     # [2007, 2460, 3448, 3373, 1141, 3319, 3098, 1429, 3895, 1172, 90, 723, 2125, 2245, 750, 1469, 1177, 493, 500, 3621, 606, 3480, 3260, 3813, 3105, 2411, 3350, 774, 2618, 4117, 922, 1173, 1802, 3472]
@@ -140,53 +151,18 @@ for Z in range(4, 5):
      # ARGS['LECs'] = 'N3LO_EM500'
 
      # EM(1.8/2.0)
-     ARGS['2bme'] = '/Users/antoinebelley/Documents/TRIUMF/Interactions/TwBME-HO_NN-only_N3LO_EM500_srg1.80_hw16_emax18_e2max36.me2j.gz'
-     ARGS['3bme'] = '/Users/antoinebelley/Documents/TRIUMF/Interactions/NO2B_ThBME_EM1.8_2.0_3NFJmax15_IS_hw16_ms18_36_24.stream.bin'
-     ARGS['LECs'] = 'EM1.8_2.0'
-
-     ARGS["3bme_type"] = "no2b"
-     # ARGS["no2b_precision"] = "half"
+     
+    
 
      ARGS['hw'] = '%d' % hw
      ARGS['A'] = '%d' % A
-     # ARGS['valence_space'] = 'fp-shell'
      # this is just a label when custom_valence_space is set
      ARGS['valence_space'] = 'p-shell'
      # ARGS['custom_valence_space'] = 'Ni56,p0f5,n0f5,p1p3,n1p3,p1p1,n1p1,p0g9,n0g9'  # AKA: Ni56 core with jj44pn
-     # ARGS['valence_space'] = '3hw-shell'
-#     ARGS['valence_space'] = 'Cr%d'%A
-#     ARGS['core_generator'] = 'imaginary-time'
-#     ARGS['valence_generator'] = 'shell-model-imaginary-time'
-     # ARGS['emax'] = '%d'%e
-#     ARGS['method'] = method
+     ARGS['Operators'] = 'Fermi'
 
-     # ARGS['Operators'] = 'GamowTeller'    # Operators to consistenly transform, separated by commas.
-     ARGS['Operators'] = 'M0nu_F_3.54_none,M0nu_GT_3.54_none,M0nu_T_3.54_none'
-#     ARGS['Operators'] = 'Rp2,Rn2'
-#     ARGS['Operators'] = 'E2'
-#     ARGS['Operators'] = 'E2,M1'
-#     ARGS['Operators'] = 'E2,M1,GamowTeller'
-#     ARGS['Operators'] = 'M1p,M1n,Sigma_p,Sigma_n'
-#     ARGS['Operators'] = 'GamowTeller'
-    #  ARGS['input_op_fmt'] = 'miyagi'
-
-     # all 6 short range operators:
-     # ARGS['OperatorsFromFile'] ='0vbbFermiVVSR^0_2_0_2^/home/alextodd/projects/rrg-holt/alextodd/NuHamil-public/exe/0vbbFermiVV_Ec_7.72_Range_SR_-N2LO-Local3-500_TwBME-HO_NN-only_N3LO_EM500_srg2.0_hw15_emax14_e2max28.me2j.gz,0vbbGamowTellerAASR^0_2_0_2^/home/alextodd/projects/rrg-holt/alextodd/NuHamil-public/exe/0vbbGamowTellerAA_Ec_7.72_Range_SR_-N2LO-Local3-500_TwBME-HO_NN-only_N3LO_EM500_srg2.0_hw15_emax14_e2max28.me2j.gz,0vbbGamowTellerAPSR^0_2_0_2^/home/alextodd/projects/rrg-holt/alextodd/NuHamil-public/exe/0vbbGamowTellerAP_Ec_7.72_Range_SR_-N2LO-Local3-500_TwBME-HO_NN-only_N3LO_EM500_srg2.0_hw15_emax14_e2max28.me2j.gz,0vbbGamowTellerPPSR^0_2_0_2^/home/alextodd/projects/rrg-holt/alextodd/NuHamil-public/exe/0vbbGamowTellerPP_Ec_7.72_Range_SR_-N2LO-Local3-500_TwBME-HO_NN-only_N3LO_EM500_srg2.0_hw15_emax14_e2max28.me2j.gz,0vbbTensorAPSR^0_2_0_2^/home/alextodd/projects/rrg-holt/alextodd/NuHamil-public/exe/0vbbTensorAP_Ec_7.72_Range_SR_-N2LO-Local3-500_TwBME-HO_NN-only_N3LO_EM500_srg2.0_hw15_emax14_e2max28.me2j.gz,0bvvTensorPPSR^0_2_0_2^/home/alextodd/projects/rrg-holt/alextodd/NuHamil-public/exe/0vbbTensorPP_Ec_7.72_Range_SR_-N2LO-Local3-500_TwBME-HO_NN-only_N3LO_EM500_srg2.0_hw15_emax14_e2max28.me2j.gz'
-
-     # two tensor operators:
-     # ARGS['OperatorsFromFile'] ='0vbbTensorAPSR^0_2_0_2^/home/alextodd/projects/rrg-holt/alextodd/NuHamil-public/exe/0vbbTensorAP_Ec_7.72_Range_SR_-N2LO-Local3-500_TwBME-HO_NN-only_N3LO_EM500_srg2.0_hw15_emax14_e2max28.me2j.gz,0bvvTensorPPSR^0_2_0_2^/home/alextodd/projects/rrg-holt/alextodd/NuHamil-public/exe/0vbbTensorPP_Ec_7.72_Range_SR_-N2LO-Local3-500_TwBME-HO_NN-only_N3LO_EM500_srg2.0_hw15_emax14_e2max28.me2j.gz'
-
-     # ARGS['OperatorsFromFile'] ='0vbbGamowTellerPPSR^0_2_0_2^/home/alextodd/projects/rrg-holt/alextodd/NuHamil-public/exe/0vbbGamowTellerPP_Ec_7.72_Range_SR_-N2LO-Local3-500_TwBME-HO_NN-only_N3LO_EM500_srg2.0_hw15_emax14_e2max28.me2j.gz'
-     # ARGS['OperatorsFromFile'] ='0vbbFermiVVSR^0_2_0_2^/home/alextodd/projects/rrg-holt/alextodd/NuHamil-public/exe/0vbbFermiVV_Ec_7.72_Range_SR_-N2LO-Local3-500_TwBME-HO_NN-only_N3LO_EM500_srg2.0_hw15_emax14_e2max28.me2j.gz'
-
-     # ARGS['OperatorsFromFile']= '0vbbTensorPPSR^0_2_0_2^/home/alextodd/projects/rrg-holt/alextodd/NuHamil-public/exe/0vbbTensorPP_Ec_7.72_Range_SR_-N2LO-Local3-500_TwBME-HO_NN-only_N3LO_EM500_srg2.0_hw15_emax14_e2max28.me2j.gz'
-
+    
     # Make an estimate of how much time to request. Only used for slurm at the moment.
-     # if   e <  5 : time_request = '00:10:00'
-     # elif e <  8 : time_request = '01:00:00'
-     # elif e < 10 : time_request = '04:00:00'
-     # elif e < 12 : time_request = '12:00:00'
-     # time_request = '100:00:00'
      if e <= 4:
        time_request = '00:06:00'
      elif e <= 6:
@@ -200,13 +176,12 @@ for Z in range(4, 5):
      elif e <= 14:
        time_request = '35:00:00'
      # time_request = '40:00:00'
-# /home/alextodd/projects/rrg-holt/alextodd/results
      jobname = '%s_%s_%s_%s_e%s_E%s_s%s_hw%s_A%s' % (
          ARGS['valence_space'], ARGS['LECs'], ARGS['method'], ARGS['reference'], ARGS['emax'], ARGS['e3max'], ARGS['smax'], ARGS['hw'], ARGS['A'])
      logname = jobname + \
          datetime.fromtimestamp(time()).strftime('_%y%m%d%H%M.log')
      print(jobname)
-  # Some optional parameters that we probably want in the output name if we're using them
+     # Some optional parameters that we probably want in the output name if we're using them
      if 'lmax3' in ARGS:
        jobname += '_l%d' % (ARGS['lmax3'])
      if 'eta_criterion' in ARGS:
@@ -221,7 +196,7 @@ for Z in range(4, 5):
      cmd = ' '.join([exe] + ['%s=%s' % (x, ARGS[x]) for x in ARGS])
      print(cmd)
 
-  # Submit the job if we're running in batch mode, otherwise just run in the current shell
+     # Submit the job if we're running in batch mode, otherwise just run in the current shell
      if batch_mode == True:
        sfile = open(jobname+'.batch', 'w')
        if BATCHSYS == 'PBS':
